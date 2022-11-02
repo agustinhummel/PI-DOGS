582d545438622294e8194620705edc800fb51e42
agustinhummel <agustinhummel7@gmail.com>
2022-11-02 19:24:38 -0300

    seven

--- client/src/components/Searchbar.jsx
+++ client/src/components/Searchbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { getNameDogs } from "../redux/actions";
