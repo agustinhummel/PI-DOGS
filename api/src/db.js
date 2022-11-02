d24b779e51eb1a5d3d2795d2b597f5ea963b0d23
agustinhummel <agustinhummel7@gmail.com>
2022-11-02 18:53:23 -0300

    second commit

--- api/src/db.js
+++ api/src/db.js
@@ -3,13 +3,23 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,
+  SERVER_DB
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-});
+const sequelize = new Sequelize(SERVER_DB,
+  {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    logging: false,
+    native: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+
+  });
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
